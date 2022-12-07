52fb0ad19661fdd96cf7bb1781cb5591c00e68f7
Imran Ansari <75426751+stasim101@users.noreply.github.com>
2022-12-07 13:28:53 +0530

    task-1: Adding interceptor
    Adding interceptor for user domain validation

---
+++ oauth-rest/oauth-resource-server/src/main/java/com/baeldung/resource/spring/UserDomainInterceptor.java
@@ -0,0 +1,46 @@
+package com.baeldung.resource.spring;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.baeldung.resource.web.controller.UserInfoController;
+
+/**
+ * @author IMRAN
+ *
+ */
+@Component
+public class UserDomainInterceptor implements HandlerInterceptor {
+
+	private static final Logger log = LoggerFactory.getLogger(UserDomainInterceptor.class);
+	private static final String ALLOWED_DOMAIN = "@test.com";
+
+	@Autowired
+	private UserInfoController userInfoController;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		final JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) request.getUserPrincipal();
+
+		final Jwt jwt = (Jwt) jwtAuthenticationToken.getCredentials();
+
+		final String userName = (String) userInfoController.getUserInfo(jwt).get("user_name");
+
+		if (userName.endsWith(ALLOWED_DOMAIN)) {
+			return true;
+		} else {
+			log.error("Invalid user domain! Allowed domain is {}", ALLOWED_DOMAIN);
+			return false;
+		}
+	}
+}
